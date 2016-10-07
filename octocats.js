fd960560b128b7af212b42954aa9de34f75a8caa
JonathanStevens17 <7JStevens@stpatrick.org>
2016-10-07 10:25:55 -0500

    made improvments

---
+++ octocats.js
@@ -0,0 +1,24 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+function Octocat(n, a){
+ this.name = n
+ this.arms = a
+
+ this.slap = fucntion();
+
+
+}
+
+
+
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
+var stevecat = new Octocat("Stevens", 5)
+var joncat = new Octocat("Jonathan", 10)
+var fatcat = new Octocat("Fatty", 15)
